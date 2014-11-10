cf7150cc937aa51af4134b7886124a12a2d5b316
Tom Mitchell <tom@tom.org>
2014-11-10 07:15:46 -0500

    change hostname diskstation1 to just diskstation

--- rails/wx-services/db/rotate-diskstation1-production.sh
+++ rails/wx-services/db/rotate-diskstation1-production.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-mysql --user wxprod -p -h diskstation1 weather_production < db/rotate_old_archive_records.sql
+mysql --user wxprod -p -h diskstation weather_production < db/rotate_old_archive_records.sql
